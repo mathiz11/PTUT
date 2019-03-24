af3333adb883f786738e685cb9c203ebe3d4f9c3
Ashrani3 <48151083+Ashrani3@users.noreply.github.com>
2019-03-24 12:27:22 +0100

    Add files via upload
    Remplacement de certains fichiers et ajout du Middleware de connexion. Attention d'installer nodemon

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//require pour middleware
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var session = require('express-session');
+var connection = require('./lib/dbconn');
+
+//liste des routes
 var connexionRouter = require('./routes/connexion');
+var mdpRouter = require('./routes/mdp_oublie');
 var questionnaireRouter = require('./routes/questionnaire');
 var sessionRouter = require('./routes/session');
 var profilRouter = require('./routes/profil');
@@ -16,12 +24,81 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+	cookieName: 'session',
+	secret: 'askthem secret',
+	resave: false,
+	saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use('local', new LocalStrategy({
+	usernameField: 'courriel',
+	passwordField: 'mdp',
+	passReqToCallback: true
+},function (req, courriel, mdp, done){
+      console.log(courriel+' = '+ mdp);
+      connection.query("select * from membres where mail = ?", [courriel], function(err, rows){
+          console.log(err);
+        if (err) return done(null, err);
+
+        if(!rows.length){
+			console.log("Longueur différente");
+			return done(null, false); 
+		}
+		
+		var dbMail = rows[0].mail;
+		var dbPassword  = rows[0].mdp;
+		if (!(dbMail == courriel) | !(dbPassword == mdp)){
+			console.log("Identifiants incorrects");
+			return done(null, false);
+		}
+        req.session.user = rows[0];
+        return done(null, rows[0]);
+    }
+)}));
+
+passport.serializeUser(function(user, done){
+    done(null, user.id_membre);
+});
+
+passport.deserializeUser(function(id, done){
+	connection.query("select * from membres where id_membre = "+ id, function (err, rows){
+        done(err, rows[0]);
+    });
+});
+
+app.get('/signin', function(req, res){
+  res.render('connexion', { title: 'AskThem'});
+});
+
+app.post('/signin', passport.authenticate('local', {
+	successRedirect: '/questionnaire',
+	failureRedirect: '/'
+}), function(req, res, info){
+	res.render('connexion', { title: 'AskThem', error: 'Identifiants erronés'});
+});
+
+function isAuthenticated(req, res, next){
+	if (req.isAuthenticated())
+		return next();
+	res.redirect('/');
+}
+
+app.get('/logout', function(req, res){
+    req.session.destroy();
+    req.logout();
+    res.redirect('/');
+});
+
 app.use('/', connexionRouter);
+app.use('/mdp', mdpRouter);
 app.use('/questionnaire', questionnaireRouter);
 app.use('/session', sessionRouter);
 app.use('/profil', profilRouter);
--- routes/questionnaire.js
+++ routes/questionnaire.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+function isAuthenticated(req, res, next) {
+  if (req.session.user)
+      return next();
+
+  // IF A USER ISN'T LOGGED IN, THEN REDIRECT THEM SIGNIN PAGE
+  res.redirect('/signin');
+}
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', isAuthenticated, function(req, res, next) {
   res.render('questionnaire', { title: 'AskThem', rubrique: 'Questionnaire' });
 });
 
