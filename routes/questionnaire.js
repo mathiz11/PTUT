307273b7ca836c84fb208eefca05a933c7906a7a
ENJOLRAS <you@example.com>
2019-03-24 22:01:00 +0100

    lister et supprimer questionnaire

--- routes/questionnaire.js
+++ routes/questionnaire.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var connection = require('../lib/dbconn');
+const Quest_Dao = require('../dao/dao_quest.js');
+const questDao = new Quest_Dao();
 
 function isAuthenticated(req, res, next) {
   if (req.session.user)
@@ -12,13 +13,24 @@ function isAuthenticated(req, res, next) {
 
 /* GET home page. */
 router.get('/', isAuthenticated, function(req, res, next) {
-  let listQ = [];
-  connection.query("select * from questionnaires where id_membre = " + req.session.user.id_membre, 
-  function(err, results, fields) {
-    if (err) throw err;
-    res.render('questionnaire', { title: 'AskThem', questionnaires: results});
-  });
-  console.log(listQ);
+  const request = async () => {
+    const results = await questDao.read_where(req.session.user.id_membre);
+    res.render('questionnaire', { title: 'AskThem', questionnaires: results, error: null});
+  }
+  request();
+});
+
+router.get('/delete/:id', isAuthenticated, function(req, res, next) {
+  const request = async () => {
+    let results = await questDao.deleteEntity(req.param("id"));
+    if(!results) {
+      const error = "<div id=\"error\">Erreur lors de la suppression de questionnaire</div>";
+      res.render('questionnaire', { title: 'AskThem', questionnaires: results, error: error});
+    }
+    results = await questDao.read_where(req.session.user.id_membre);
+    res.render('questionnaire', { title: 'AskThem', questionnaires: results, error: null});
+  }
+  request();
 });
 
 module.exports = router;
