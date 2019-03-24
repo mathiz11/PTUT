1b562a9eed337f9de78bea05293f04a7d1dfbfca
braco-robberto-69 <48218503+braco-robberto-69@users.noreply.github.com>
2019-03-24 18:19:30 +0100

    Add files via upload

---
+++ dao/dao_user.js
@@ -0,0 +1,112 @@
+const dbConnection = require("./connexion");
+const queries = require("../queries/user_queries");
+
+module.exports = class UserDao {
+  async saveEntity(entity) {
+    let con = await dbConnection();
+    try {
+      await con.query("START TRANSACTION");
+      let savedUser = await con.query(
+        queries.insert_user,
+        [entity.id_membre, entity.nom, entity.mail, entity.mdp]
+      );
+      await con.query("COMMIT");
+      entity.id_membre = savedUser.insertId;
+      return entity;
+    } catch (ex) {
+      await con.query("ROLLBACK");
+      console.log(ex);
+      throw ex;
+    } finally {
+      await con.release();
+      await con.destroy();
+    }
+  }
+
+  async updateEntity(entity) {
+    let con = await dbConnection();
+    try {
+      await con.query("START TRANSACTION");
+      await con.query(queries.update_user, [
+        entity.mail,
+        entity.mdp,
+        entity.id
+      ]);
+      await con.query("COMMIT");
+      return true;
+    } catch (ex) {
+      await con.query("ROLLBACK");
+      console.log(ex);
+      throw ex;
+    } finally {
+      await con.release();
+      await con.destroy();
+    }
+  }
+
+  async deleteEntity(id) {
+    let con = await dbConnection();
+    try {
+      await con.query("START TRANSACTION");
+      await con.query(queries.delete_user, [id]);
+      await con.query("COMMIT");
+      return true;
+    } catch (ex) {
+      await con.query("ROLLBACK");
+      console.log(ex);
+      throw ex;
+    } finally {
+      await con.release();
+      await con.destroy();
+    }
+  }
+
+  async readEntities() {
+    let con = await dbConnection();
+    try {
+      await con.query("START TRANSACTION");
+      let user = await con.query(queries.read_user);
+      await con.query("COMMIT");
+      user = JSON.parse(JSON.stringify(user));
+      return user;
+    } catch (ex) {
+      console.log(ex);
+      throw ex;
+    } finally {
+      await con.release();
+      await con.destroy();
+    }
+  };
+
+  async read_where(mail, mdp) {
+    let con = await dbConnection();
+    try {
+      await con.query("START TRANSACTION");
+      let user = await con.query(queries.read_user_where, [mail, mdp]);
+      await con.query("COMMIT");
+      return user;
+    } catch (ex) {
+      console.log(ex);
+      throw ex;
+    } finally {
+      await con.release();
+      await con.destroy();
+    }
+  };
+
+  async read_user_pwd(mail) {
+    let con = await dbConnection();
+    try {
+      await con.query("START TRANSACTION");
+      let user = await con.query(queries.read_user_pwd, [mail]);
+      await con.query("COMMIT");
+      return user;
+    } catch (ex) {
+      console.log(ex);
+      throw ex;
+    } finally {
+      await con.release();
+      await con.destroy();
+    }
+  };
+};
---
+++ queries/user_queries.js
@@ -0,0 +1,8 @@
+module.exports = {
+    insert_user: `INSERT INTO membres(id_membre, nom, mail, mdp) VALUES(?, ?, ?, ?)`,
+    read_user: `SELECT * FROM membres`,
+	  read_user_where: `SELECT * FROM membres WHERE mail=? AND mdp=?`,
+    read_user_pwd: `SELECT mdp FROM membres WHERE mail=?`,
+    update_user: `UPDATE membres SET membres.mail = ?, membres.mdp = ? WHERE membres.id_membre = ?`,
+    delete_user: `DELETE FROM membres WHERE membres.id_membre = ?`
+}
