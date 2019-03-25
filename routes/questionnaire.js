e2477e643642f0bd38d4069ac571dda982453794
Endrelou <48223036+Endrelou@users.noreply.github.com>
2019-03-25 02:37:14 +0100

    Update questionnaire.js

--- routes/questionnaire.js
+++ routes/questionnaire.js
@@ -20,17 +20,46 @@ router.get('/', isAuthenticated, function(req, res, next) {
   request();
 });
 
-router.get('/delete/:id', isAuthenticated, function(req, res, next) {
+router.get('/delete/:idQnair', isAuthenticated, function(req, res, next) {
   const request = async () => {
-    let results = await questDao.deleteEntity(req.param("id"));
+    let results = await questDao.deleteEntity(req.param.idQnair);
     if(!results) {
       const error = "<div id=\"error\">Erreur lors de la suppression de questionnaire</div>";
       res.render('questionnaire', { title: 'AskThem', questionnaires: results, error: error});
     }
-    results = await questDao.read_where(req.session.user.id_membre);
-    res.render('questionnaire', { title: 'AskThem', questionnaires: results, error: null});
+  }
+  request();
+  res.redirect('/questionnaire')
+});
+
+router.get('/insert', isAuthenticated, function(req, res, next) {
+  const request = async () => {
+    let results = await questDao.saveEntity(req.session.user.id_membre,'undef',new Date(), new Date(), 0);
+    if(!results) {
+      const error = "<div id=\"error\">Erreur lors de l'ajout de questionnaire</div>";
+      res.render('questionnaire', { title: 'AskThem', questionnaires: results, error: error});
+    }
+  }
+  request();
+  res.redirect('/questionnaire');
+});
+
+router.get('/:idQnair', isAuthenticated, function(req, res, next) {
+  const request = async () => {
+    let results = await questDao.read_quest_where(req.param.id);
+    res.render('monQuestionnaire', { title: 'AskThem', element: results, error: null});
   }
   request();
 });
+
+router.get('/:idQnair/question', isAuthenticated, function(req, res, next) {
+  const request = async () => {
+    let results = await questDao.select_questions(req.param.idQnair);
+    res.render('mesQuestions', { title: 'AskThem', id_questionnaire: req.param.idQnair ,questions: results, error: null});
+  }
+  request();
+});
+
+
 
 module.exports = router;
