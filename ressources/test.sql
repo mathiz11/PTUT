fae390fb2d9dbc6f93e12dc818384c8afe25ddd5
Ashrani3 <48151083+Ashrani3@users.noreply.github.com>
2019-03-25 02:39:28 +0100

    Add files via upload

---
+++ ressources/test.sql
@@ -0,0 +1,13 @@
+insert into questions values (default,1,default,'Avez-vous compris ?','QCO',0);
+insert into questions values (default,1,default,'Quelle note pour ce cours ?','QCU',0);
+insert into questions values (default,1,'default','Que changeriez-vous ?','QCM',0);
+
+insert into reponsesouvertes values (default,1,default,'Oui j ai compris le cours.');
+insert into reponsesouvertes values (default,1,default,'Non j aimerai avoir plus d explications.');
+insert into reponsesouvertes values (default,1,default,'Je comprendrai en faisant les TDs.');
+insert into questions values (default,2,'undef2','2019-03-20','2019-03-20',0);
+insert into questions values (default,2,'undef3','2019-03-20','2019-03-20',0);
+
+insert into questions values (default,3,'undef','2019-03-20','2019-03-20',0);
+insert into questions values (default,3,'undef2','2019-03-20','2019-03-20',0);
+insert into questions values (default,3,'undef3','2019-03-20','2019-03-20',0);
