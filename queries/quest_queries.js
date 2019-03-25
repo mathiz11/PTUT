c61fe2a3f4c11e16fd87e73cc4eadbbd28e360a8
Endrelou <48223036+Endrelou@users.noreply.github.com>
2019-03-25 04:04:54 +0100

    Update quest_queries.js

--- queries/quest_queries.js
+++ queries/quest_queries.js
@@ -2,8 +2,8 @@ module.exports = {
     insert_quest: `INSERT INTO questionnaires(id_questionnaire, id_membre, intitule_quest, date_crea, date_modif, tps_attente) VALUES(default, ?, ?, ?, ?, ?)`,
     read_quest: `SELECT * FROM questionnaires`,
 	  read_quest_where: `SELECT * FROM questionnaires WHERE id_membre=?`,
-    read_maquest: `SELECT * FROM questionnaires WHERE id_questionnaire=?`,
-    update_quest: `UPDATE questionnaires SET questionnaires.intitule_quest = ?, questionnaires.date_modif = ?, questionnaires.tps_attente = ? WHERE questionnaires.id_questionnaire = ?`,
+    read_monquest: `SELECT * FROM questionnaires WHERE id_questionnaire=?`,
+    update_quest: `UPDATE questionnaires SET questionnaires.intitule_quest = ?, questionnaires.date_modif = ? WHERE questionnaires.id_questionnaire = ?`,
     delete_quest: `DELETE FROM questionnaires WHERE questionnaires.id_questionnaire = ?`,
-    read_question: `Select * FROM questions where id_questionnaire = ?`
+    read_question: `SELECT * FROM questions WHERE questions.id_questionnaire = ?`
 }
